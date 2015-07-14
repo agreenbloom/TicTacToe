287f369e0b4e433e7fcf82d4dba41d422633e7c7
Abby Greenbloom <abby.greenbloom@gmail.com>
2015-07-14 16:25:23 -0400

    refactored JavaScript code and CSS

--- script.js
+++ script.js
@@ -2,76 +2,89 @@ var canvas;
 var user1 = "user1";
 var user2 = "user2";
 
-
-
-
 $(document).ready(function() {
-var isUserTurn1 = true;
-var user1Play = [];
-var user2Play = [];
-var winningCombinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 
-  $('td').click(function () {
-    if ($(this).hasClass('user1') || $(this).hasClass('user2')){
-      return;
-    }
-    if (!isUserTurn1) {
-        $(this).addClass('user1');
-        isUserTurn1 = true;
-        // alert("Player 1");
+  var isUserTurn1 = true;
+  var user1Play = [];
+  var user2Play = [];
+  var winningCombinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+  var turn = 'user1'
+
+  var switchTurns = function () {
+    if (turn == "user1") {
+      turn = "user2"
     } else {
-        $(this).addClass('user2');
-        isUserTurn1 = false;
-        // alert("Player 2")
+      turn = "user1"
     }
+  }
+
+  var someoneWon = function(){
+    $('td').off("click");
+    window.alert(turn + " won");
+  }
 
 
+  var checkWin = function() {
+    console.log("--------------check win-------------------")
+    var userCells = $( "."+turn ).map(function() { return this.id; });
     for (var i = 0; i < winningCombinations.length; i++) {
       var hasCompleteSet = true;
       var CurrentWinningCombination = winningCombinations[i];
-
-      var userCells = $( ".user1" ).map(function() {
-        return this.id;
-      });
-
+      console.log(CurrentWinningCombination)
+      // Code to Check User #1 winning?
       for (var arr = 0; arr < CurrentWinningCombination.length; arr++) {
         var singleCell = CurrentWinningCombination[arr];
-
         var userHasCell = $.inArray(singleCell.toString(), userCells) ;
+        console.log(userHasCell)
         if(userHasCell == -1) {
           hasCompleteSet = false
         }
       }
 
-      if(hasCompleteSet){
-        someoneWon("user 1");
-      } else {
-
-        hasCompleteSet = true;
-        var userCells = $( ".user2" ).map(function() {
-          return this.id;
-        });
-
-        for (var arr = 0; arr < CurrentWinningCombination.length; arr++) {
-          var singleCell = CurrentWinningCombination[arr];
-
-          var userHasCell = $.inArray(singleCell.toString(), userCells) ;
-          if(userHasCell == -1) {
-            hasCompleteSet = false
-          }
-        }
-
-        if(hasCompleteSet){
-          someoneWon("user 2");
-        }
+      if (hasCompleteSet === true){
+        someoneWon();
+        return
       }
+
     }
-  });
-
-  function someoneWon(winner){
-    $('td').unbind("click");
-    window.alert(winner + " won");
-
   }
+
+  $('td').on('click', function () {
+    // if ($(this).hasClass('user1') || $(this).hasClass('user2')){
+    //   return;
+    // }
+
+    $(this).addClass(turn)
+    $(this).off('click')
+
+    checkWin()
+
+    // Switch turns
+    switchTurns()
+
+    //   if(hasCompleteSet){
+    //     someoneWon("user 1");
+    //   } else {
+    //     // Code to check User #2 winning?
+    //     hasCompleteSet = true;
+    //     var userCells = $( ".user2" ).map(function() {
+    //       return this.id;
+    //     });
+
+    //     for (var arr = 0; arr < CurrentWinningCombination.length; arr++) {
+    //       var singleCell = CurrentWinningCombination[arr];
+
+    //       var userHasCell = $.inArray(singleCell.toString(), userCells) ;
+    //       if(userHasCell == -1) {
+    //         hasCompleteSet = false
+    //       }
+    //     }
+
+    //     if(hasCompleteSet){
+    //       someoneWon("user 2");
+    //     }
+    //   }
+    // }
+  });
 
 })
